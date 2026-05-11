b78b75d613ec7706be33099532af758e448d3e54
Subodh Kumar <162813692+SubodhK143@users.noreply.github.com>
2026-05-11 08:13:45 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : "database-1-instance-1.ctieyicq2cj3.ap-south-1.rds.amazonaws.com",
+    DB_USER : "admin",
+    DB_PWD : "subodhdatabase",
+    DB_DATABASE : "webappdb",
+});
